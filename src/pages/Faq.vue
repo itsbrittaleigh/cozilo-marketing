0a01da1d937ff00f8bb4873dd2b4566f49414bb6
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2018-07-12 17:01:06 -0400

    restyling faq page

--- src/pages/Faq.vue
+++ src/pages/Faq.vue
@@ -19,15 +19,11 @@
       </hero-section>
       <section class="faq">
         <div class="container content-container content padded-section">
-          <p class="lead fade-in">
-            Below is a list of frequently asked questions that Cozilo users have about
-            signing up, payment options, and more. If you don't see an answer to your
-            question below, reach out to us using our
-            <router-link :to="{ name: 'Contact' }">contact form</router-link>.
-          </p>
-          <div class="accordion hidden">
+          <div class="accordion fade-in">
+            <p class="heading">My Account</p>
             <v-collapse-wrapper :active="true">
               <div class="header" v-collapse-toggle>
+                <img src="../assets/images/icons/arrow-blue-down.png" alt="">
                 <h2 class="color-lightblue">How can I sign up for Cozilo?</h2>
               </div>
               <div class="my-content" v-collapse-content>
@@ -40,7 +36,25 @@
             </v-collapse-wrapper>
             <v-collapse-wrapper>
               <div class="header" v-collapse-toggle>
-                <h2 class="color-orange">What are my payment options?</h2>
+                <img src="../assets/images/icons/arrow-blue-down.png" alt="">
+                <h2 class="color-lightblue">Who do I contact with account questions?</h2>
+              </div>
+              <div class="my-content" v-collapse-content>
+                <p>
+                  If you have any account questions, feel free to
+                  <router-link :to="{ name: 'Contact' }">
+                    contact our customer support team</router-link>.
+                  Fill out the form, give us a call, or email us today.
+                </p>
+              </div>
+            </v-collapse-wrapper>
+          </div>
+          <div class="accordion">
+            <p class="heading">Transactions and Payments</p>
+            <v-collapse-wrapper>
+              <div class="header" v-collapse-toggle>
+                <img src="../assets/images/icons/arrow-green-down.png" alt="">
+                <h2 class="color-green">What are my payment options?</h2>
               </div>
               <div class="my-content" v-collapse-content>
                 <p>
@@ -52,6 +66,7 @@
             </v-collapse-wrapper>
             <v-collapse-wrapper>
               <div class="header" v-collapse-toggle>
+                <img src="../assets/images/icons/arrow-green-down.png" alt="">
                 <h2 class="color-green">How do Cozilo tokens work?</h2>
               </div>
               <div class="my-content" v-collapse-content>
@@ -65,7 +80,8 @@
             </v-collapse-wrapper>
             <v-collapse-wrapper>
               <div class="header" v-collapse-toggle>
-                <h2 class="color-darkblue">How does the trade system work?</h2>
+                <img src="../assets/images/icons/arrow-green-down.png" alt="">
+                <h2 class="color-green">How does the trade system work?</h2>
               </div>
               <div class="my-content" v-collapse-content>
                 <p>
@@ -78,18 +94,8 @@
             </v-collapse-wrapper>
             <v-collapse-wrapper>
               <div class="header" v-collapse-toggle>
-                <h2 class="color-lightblue">Does Cozilo manage shipping?</h2>
-              </div>
-              <div class="my-content" v-collapse-content>
-                <p>
-                  Yes, Cozilo handles all shipping through Shippo, a cost-efficient,
-                  cloud-based, e-commerce platform.
-                </p>
-              </div>
-            </v-collapse-wrapper>
-            <v-collapse-wrapper>
-              <div class="header" v-collapse-toggle>
-                <h2 class="color-orange">How are my transactions secured?</h2>
+                <img src="../assets/images/icons/arrow-green-down.png" alt="">
+                <h2 class="color-green">How are my transactions secured?</h2>
               </div>
               <div class="my-content" v-collapse-content>
                 <p>
@@ -100,16 +106,18 @@
                 </p>
               </div>
             </v-collapse-wrapper>
+          </div>
+          <div class="accordion">
+            <p class="heading">Shipping</p>
             <v-collapse-wrapper>
               <div class="header" v-collapse-toggle>
-                <h2 class="color-green">Who do I contact with account questions?</h2>
+                <img src="../assets/images/icons/arrow-orange-down.png" alt="">
+                <h2 class="color-orange">Does Cozilo manage shipping?</h2>
               </div>
               <div class="my-content" v-collapse-content>
                 <p>
-                  If you have any account questions, feel free to
-                  <router-link :to="{ name: 'Contact' }">
-                    contact our customer support team</router-link>.
-                  Fill out the form, give us a call, or email us today.
+                  Yes, Cozilo handles all shipping through Shippo, a cost-efficient,
+                  cloud-based, e-commerce platform.
                 </p>
               </div>
             </v-collapse-wrapper>
@@ -142,15 +150,32 @@ export default {
 
 <style lang="scss" scoped>
 @import '../assets/styles/variables';
+.heading {
+  @include title-font;
+  margin: 60px 28px 0;
+  font-size: 12px;
+  color: $darkblue;
+}
 .accordion {
+  &:first-of-type {
+    .heading {
+      margin-top: 0;
+    }
+  }
   .vc-collapse {
     border-bottom: 1px solid #e5e5e5;
   }
   .header {
-    padding: 10px 0;
+    padding: 20px 0;
+    display: flex;
+    align-items: center;
     cursor: pointer;
+    img {
+      margin-right: 10px;
+    }
     h2 {
       font-weight: normal;
+      text-transform: none;
     }
   }
 }
