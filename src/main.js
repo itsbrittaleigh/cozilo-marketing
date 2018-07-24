11d1264260ddcea2aa51d17d9664a6ad82acb31f
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2018-07-24 11:15:48 -0400

    adding in meta information

--- src/main.js
+++ src/main.js
@@ -18,7 +18,22 @@ Vue.directive('in-viewport', InViewportDirective);
 
 router.beforeEach((to, from, next) => {
   store.commit('closeMenu');
-  next();
+  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title);
+  const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
+  if (nearestWithTitle) document.title = nearestWithTitle.meta.title;
+  Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el));
+  if (!nearestWithMeta) return next();
+
+  nearestWithMeta.meta.metaTags.map((tagDef) => {
+    const tag = document.createElement('meta');
+    Object.keys(tagDef).forEach((key) => {
+      tag.setAttribute(key, tagDef[key]);
+    });
+    tag.setAttribute('data-vue-router-controlled', '');
+    return tag;
+  }).forEach(tag => document.head.appendChild(tag));
+
+  return next();
 });
 
 /* eslint-disable no-new */
--- src/router/index.js
+++ src/router/index.js
@@ -20,31 +20,85 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home,
+      meta: {
+        title: 'Cozilo: The Truly B2B Mobile App For Buying & Selling Goods',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Cozilo is a truly B2B mobile marketplace app - buy & sell goods, interact with local businesses, & advertise your company, all within the app. Download it today!',
+          },
+        ],
+      },
     },
     {
       path: '/about',
       name: 'About',
       component: About,
+      meta: {
+        title: 'The Future of B2B Transactions: Our Story | Cozilo',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Cozilo was created by a team of small business owners with small business needs in mind. Read more here about why it\'s the future of B2B transactions.',
+          },
+        ],
+      },
     },
     {
       path: '/contact',
       name: 'Contact',
       component: Contact,
+      meta: {
+        title: 'Get In Touch With Us: Contact Information | Cozilo',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Contact us today if you have questions about the Cozilo app, your account, recent transactions, or want to provide feedback on the app.',
+          },
+        ],
+      },
     },
     {
-      path: '/faq',
+      path: '/frequently-asked-questions',
       name: 'FAQ',
       component: Faq,
+      meta: {
+        title: 'Frequently Asked Questions: Accounts, Transactions, Sign Up | Cozilo',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Have a question about your Cozilo account, payment methods, or a recent transaction? Check out our list of frequently asked questions for assistance.',
+          },
+        ],
+      },
     },
     {
       path: '/features',
       name: 'Features',
       component: Features,
+      meta: {
+        title: 'Local Selling App Features: Buy, Sell, Advertise, Chat | Cozilo',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Buy, sell, and advertise your business on Cozilo, the first truly mobile B2B mobile marketplace app. Learn more about our local selling app and its features here.',
+          },
+        ],
+      },
     },
     {
       path: '/pricing',
       name: 'Pricing',
       component: Pricing,
+      meta: {
+        title: 'Cozilo App Pricing Tiers: Free, Plus, & Enterprise | Cozilo',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Interested in buying and selling on the Cozilo app? Find the pricing level for your business here and sign up today!',
+          },
+        ],
+      },
     },
   ],
 });
